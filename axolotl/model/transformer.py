6381a1298d8b76ffa9be1d6daef99b752ff8609d
Kasper Krunderup Jakobsen <kasperkrunderup@gmail.com>
2025-05-21 12:48:47 +0200

    remove comment, add lengthplot notebook

--- axolotl/model/transformer.py
+++ axolotl/model/transformer.py
@@ -208,10 +208,6 @@ class DiscreteDiTBlock(nn.Module):
 
 class EmbeddingLayer(nn.Module):
     def __init__(self, dim, vocab_dim):
-        """
-        Mode arg: 0 -> use a learned layer, 1 -> use eigenvectors, 
-        2-> add in eigenvectors, 3 -> use pretrained embedding matrix
-        """
         super().__init__()
         # # old implementation
         # self.embedding = nn.Parameter(torch.empty((vocab_dim, dim)))
