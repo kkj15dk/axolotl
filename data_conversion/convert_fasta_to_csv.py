f996813c687a5ebc77097712477c5a2d197eb2e4
Kasper Krunderup Jakobsen <kasperkrunderup@gmail.com>
2025-08-11 11:21:04 +0200

    conversion script updates

--- data_conversion/convert_fasta_to_csv.py
+++ data_conversion/convert_fasta_to_csv.py
@@ -14,20 +14,23 @@ def convert_fasta_to_csv(fasta_path, output_path, filename):
 
         for i, record in enumerate(SeqIO.parse(f"{fasta_path}", "fasta")):
             clusterid = i + 1
-            header, domainid = record.id.split('|Domain:')
-            accession = header.split(' ')[0]
+            
+            domainid = record.description.split('|Domain:')[-1]
+            accession = record.id
             output_handle.write(f"{clusterid},{accession},{domainid},{record.seq}\n")
 
 # %%
 if __name__ == "__main__":
     
     INPUT = '/mnt/e/uniref50_with_domain.fasta'
-    OUTPUT = '/datasets/'
-    OUTFILE = 'test.csv'
+    OUTPUT = '/mnt/e/'
+    OUTFILE = 'uniref50_unclustered.csv'
 
     parser = argparse.ArgumentParser(description="Convert a folder of fasta files to a csv file")
     parser.add_argument("--input", default=INPUT, type=str)
     parser.add_argument("--output", default=OUTPUT, type=str)
     parser.add_argument("--outfile", default=OUTFILE, type=str)
 
-    convert_fasta_to_csv(parser.input, parser.output, parser.outfile)
+    args = parser.parse_args()
+
+    convert_fasta_to_csv(args.input, args.output, args.outfile)
